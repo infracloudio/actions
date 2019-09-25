d83ed4292c2c595ebae334f31c1b47adc8ba4704
mugdha-adhav <mugdha.adhav@gmail.com>
2019-09-25 14:39:41 +0530

    Updated worklow and entrypoint

--- cmd/entrypoint.sh
+++ cmd/entrypoint.sh
@@ -1,11 +1,26 @@
-#!/bin/sh -l
+#!/bin/bash
+#temp branch created by actions
+branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+bname=$(echo $branch | awk '{n=split($0,a); print a[n]}') 
+branch=$(echo $bname | awk '{split($0,b,")");print b[1]}')
 
-# branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
-# bname=$(echo $branch | awk '{n=split($0,a); print a[n]}') 
-# branch=$(echo $bname | awk '{split($0,b,")");print b[1]}')
-# echo $branch
-commitID=$(git log --format="%H" -n 1)
-echo $commitID
-git checkout master 
-git cherry-pick $commitID
-#git push origin master
+#Find PR number from $branch
+pr_id=$(echo $branch | awk '{split($0,a,"/");print a[2]}')
+
+#set new branch name
+new_branch="test/$pr_id"
+
+#check if new branch already exists
+git checkout master
+branch_list=`git branch`
+if_exists=$(echo $branch_list | grep $new_branch) 
+if [ "$if_exists" != "" ]; then 
+    echo "Deleting branch"
+    git branch -D $new_branch
+    
+fi
+
+git checkout $branch
+git checkout -b $new_branch $branch 
+git push -f https://${USER_NAME}:${PASSWORD}@github.com/mugdha-adhav/sample-workflow-test.git
+echo ${MY_NAME}
